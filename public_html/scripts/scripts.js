3c9d4565917bdc0a7b3199fe020cffb389994693
heliobentes <heliobentes@gmail.com>
2017-11-20 18:50:35 -0800

    styling the layout

--- public_html/scripts/scripts.js
+++ public_html/scripts/scripts.js
@@ -1,4 +1,18 @@
 /**
  * Created by heliobentes on 6/21/17.
  */
-let a;
+
+$(document).ready(function () {
+    //menu toggle
+    $('#main-nav > li > a').on('click', function () {
+        $('#main-nav > li').removeClass('active');
+        $('#main-nav > li > ul').removeClass('open');
+        $(this).parent().addClass('active');
+        $(this).parent().find('ul').addClass('open');
+    });
+    //active link
+    $('#main-nav > li > ul > li > a').on('click', function () {
+        $('#main-nav > li > ul > li').removeClass('active');
+        $(this).parent().addClass('active');
+    });
+});
